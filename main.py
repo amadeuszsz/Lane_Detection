38ea5e0797832642b7149bc877f25a4439346280
amadeuszsz <amadeuszszymko@gmail.com>
2020-06-18 22:03:00 +0100

    Added video path from console

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-import cv2
+'''
+Simple lane detection algorithm.
+To use type "python3 main.py path_fo_video_file".
+'''
 import time
+import sys
+import cv2
+from lane_detection import LaneDetection
 
-# Video variables
-cap = cv2.VideoCapture('videos/road01.mp4')
-fps = cap.get(cv2.CAP_PROP_FPS)
-
-def sync_fps(time_start):
+def sync_fps(time_start, fps):
     '''
     Sleeping function. Wait until reach desired frame rate.
     :param time_start: last frame timestamp
     :return:
     '''
-    timeDiff = time.time() - time_start
-    if (timeDiff < 1.0 / (fps)):
-        time.sleep(1.0 / (fps) - timeDiff)
+    time_difference = time.time() - time_start
+    if time_difference < 1.0 / (fps):
+        time.sleep(1.0 / (fps) - time_difference)
 
 def main():
     '''
@@ -21,16 +23,22 @@ def main():
     Press 'q' to exit and 'w' to pause video.
     :return:
     '''
-    while (cap.isOpened()):
+    if len(sys.argv) > 1:
+        cap = cv2.VideoCapture(sys.argv[1])
+    else:
+        cap = cv2.VideoCapture('videos/road01.mp4')
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    while cap.isOpened():
         time_start = time.time()
-        ret, frame = cap.read()
+        _, frame = cap.read()
+        frame = LaneDetection(frame).lane_detection()
         cv2.imshow("Lane Detection", frame)
         key = cv2.waitKey(1)
         if key == ord('q'):
             break
         if key == ord('w'):
             cv2.waitKey(-1)
-        sync_fps(time_start)
+        sync_fps(time_start, fps)
     cap.release()
     cv2.destroyAllWindows()
 
